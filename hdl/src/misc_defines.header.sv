2fb80e321602341baa9ae4660689c2eeeee1f60a
fl4shk <fl4shk@users.noreply.github.com>
2018-05-23 10:41:52 -0400

    options_defines.header.sv

--- hdl/src/misc_defines.header.sv
+++ hdl/src/misc_defines.header.sv
@@ -6,21 +6,6 @@
 `define WIDTH_TO_MSB_POS(some_width) ((some_width) - 1)
 `define ARR_SIZE_TO_LAST_INDEX(some_size) ((some_size) - 1)
 
-`ifdef ICARUS
-`define OPT_DEBUG
-`define OPT_DEBUG_INSTR_DECODER
-`define OPT_DEBUG_MEM_ACCESS
-`define OPT_DEBUG_REGISTER_FILE
-//`define OPT_HAVE_SINGLE_CYCLE_MULTIPLY
-`endif		// ICARUS
-
-// Temporary
-`define OPT_DEBUG_REGISTER_FILE
-
-`define OPT_HAVE_STAGE_REGISTER_READ
-
-`ifndef OPT_HAVE_STAGE_REGISTER_READ
-`define OPT_HAVE_SINGLE_CYCLE_MULTIPLY
-`endif		// OPT_HAVE_STAGE_REGISTER_READ
+`include "src/options_defines.header.sv"
 
 `endif		// src__slash__misc_defines_header_sv
---
+++ hdl/src/options_defines.header.sv
@@ -0,0 +1,18 @@
+// src/options_defines.header.sv
+
+`ifdef ICARUS
+`define OPT_DEBUG
+`define OPT_DEBUG_INSTR_DECODER
+`define OPT_DEBUG_MEM_ACCESS
+`define OPT_DEBUG_REGISTER_FILE
+//`define OPT_HAVE_SINGLE_CYCLE_MULTIPLY
+`endif		// ICARUS
+
+// Temporary
+`define OPT_DEBUG_REGISTER_FILE
+
+`define OPT_HAVE_STAGE_REGISTER_READ
+
+`ifndef OPT_HAVE_STAGE_REGISTER_READ
+`define OPT_HAVE_SINGLE_CYCLE_MULTIPLY
+`endif		// OPT_HAVE_STAGE_REGISTER_READ
